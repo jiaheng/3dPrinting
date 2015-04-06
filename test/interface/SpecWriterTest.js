f6cddcbf87876c10bf687066fdeba5461d50a49b
jhe3 <jhe3@cs.st-andrews.ac.uk>
2015-04-06 01:37:20 +0100

    clean up add some test

--- test/interface/SpecWriterTest.js
+++ test/interface/SpecWriterTest.js
@@ -1,6 +1,14 @@
 var test = require('unit.js');
 var should = require('should');
 var Config = require('../Config.js');
-var SpecificationWriter = require(__dirname + '/../../src/interface/SpecificationWriter.js').Drawer;
+var SpecificationWriter = require(__dirname + '/../../src/interface/SpecificationWriter.js').SpecificationWriter;
 
-//TODO : some tests
+describe('SpecificationWriter', function() {
+	var specWriter;
+	
+	beforeEach(function() {
+		specWriter = new SpecficaationWriter(Config.specFileTarger);
+	})
+	
+	
+})
